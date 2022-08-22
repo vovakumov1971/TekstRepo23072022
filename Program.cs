88dbef3d1181e671719e654ef8239e6dd5695714
vovakumov1971 <109413527+vovakumov1971@users.noreply.github.com>
2022-08-22 11:51:24 +0300

    3 commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿{
+ 
+        int a, b;
+ 
+        Console.WriteLine("Введите два числа : ");
+ 
+        a = Convert.ToInt32(Console.ReadLine());
+ 
+        b = Convert.ToInt32(Console.ReadLine());
+ 
+        if (a > b)
+ 
+        {
+ 
+            Console.WriteLine("{0} наибольшее число", a);
+ 
+        }
+ 
+        else
+ 
+        {
+ 
+            Console.WriteLine("{0} наибольшее число ", b);
+ 
+        }
+ 
+        Console.ReadLine();
+ 
+    }
